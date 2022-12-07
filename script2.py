da4edc437b9e5687cade995f4f0cedbfed0ee178
msh1307 <86157260+msh1307@users.noreply.github.com>
2022-12-07 20:24:36 +0900

    Create script2.py

---
+++ script2.py
@@ -0,0 +1,47 @@
+import gdb
+import re
+
+libc_base = 0
+start = 0
+
+gdb.execute("file ./prob")
+gdb.execute("set disable-randomization off ")
+gdb.execute("b * main+141")
+while (True):
+    gdb.execute("r")
+    mem = gdb.execute("vmmap libc",to_string=True) # -> for provided libc
+    p = re.compile("0x")
+    idx = re.search(p,mem).start()
+    libc_base = int(mem[idx:idx+18],16)
+    main_arena = libc_base + 0x1ecbd8 #+ 0x1ecb80 
+    start = main_arena& 0xfffffffffffff000
+    print("start : "+hex(start))
+    if(start & 0x000000000000f000 == 0):
+        break
+strlen = libc_base + 0x1886d0
+
+i = 0x21
+gdb.execute("set {long long}"+hex(start+0xa8)+'='+hex((strlen & 0xffffffffffff0000)+i))
+print(hex((strlen & 0xffffffffffff0000)+i))
+gdb.execute("del breakpoints")
+gdb.execute("set $rcx = &__GI___libc_read+18")
+gdb.execute("c")
+# main_arena = gdb.parse_and_eval("&main_arena") #-> for local libc
+# start = (main_arena.cast(gdb.lookup_type("unsigned long long")) + 88) & 0xfffffffffffff000
+
+
+# i = start + 0x110   
+# for j in range(20):
+#     gdb.execute("set {long long int}%d = 0x7f"%i)
+#     print(hex(i) + ' = 0x7f')
+#     i+=8
+    
+# printf strlen 040
+# puts strlen 0a8
+
+'''
+gef➤  x/20xi 0x00007f3ddd490007
+   0x7f3ddd490007:      mov    eax,DWORD PTR [rcx+rax*4]
+   0x7f3ddd49000a:      sub    eax,edx
+   0x7f3ddd49000c:      ret
+'''
